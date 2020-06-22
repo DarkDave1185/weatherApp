a332ae0f48e193221e6610a259c1bbdea8e6d696
DarkDave <darkdave1185@gmail.com>
2020-06-22 13:03:30 -0700

    update app to pull forecast. having trouble with result after new code added. seeking assistance

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # weatherApp
+Create a weatherApp that will tell the user the weather for the area searched. 
+
+## About
+The user can getdaily weather and the 5-day forecast, including weather conditions, wind speed, uv index, and feels like temperature. The weather will give some extra information to be used at the users discretion. The uv index wll have color coded information for the user to easily identify the status. Each day represented will have an icon of the weather description for that day.
+
+## Used-
+
+*Bootstrap || OpenWeather APIdoc
+*YouTube: Web Dev Simplified & Brent Farris & Shanjai Raj || 
+*Slack - #learning-assistants -
+
+## Deployment Link- 
+https://github.com/DarkDave1185/randomPasswordGenerator
--- weatherFunction.js
+++ weatherFunction.js
@@ -1,28 +1,65 @@
+/*WEATHER HTML CONVERSION*/
 let button=document.querySelector(".submit");
 let input=document.querySelector(".input_text");
 let main=document.querySelector("#name");
 let temp=document.querySelector(".temp");
 let desc=document.querySelector(".desc");
 let clouds=document.querySelector(".clouds");
+/*FORECAST HTML CONVERSION*/
+let tempA=document.querySelector(".tempA");
+let minMaxA=document.querySelector(".minMaxA");
+let dateA=document.querySelector(".dateA");
+let windA=document.querySelector(".windA");
 
 
-/* pull from WEATHER API*/
+
+
+
+/* pull from WEATHER API*/ /*WEATHER*/
 button.addEventListener("click", function(){
     fetch("https://api.openweathermap.org/data/2.5/weather?q="
     +input.value+
     "&units=imperial&appid=b06ca37a6a6f13ebeaf5ae4e7ea80dd0")
-    .then(response => response.json())
+    ().then(response => (response.json())
     .then(data => {
-        
+        /*WEATHER VARIABLES*/
         let tempValue = data["main"]["temp"];
         let nameValue = data["name"];
         let descValue = data["weather"][0]["description"];
-        
+        /*VARIABLE CONVERSION*/
         main.innerHTML = nameValue;
         desc.innerHTML = "Description - "+descValue;
-        temp.innerHTML = "Temperature - "+tempValue;
+        temp.innerHTML = "Temperature - "+tempValue+"°F";
         input.value ="";
 })
 
+.catch(err => alert("Wrong city info!")))
+})
+/*pull from WEATHER API*/ /*FORECAST*/
+button.addEventListener("click", function(){
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q="
+    +input.value+
+    "&units=imperial&appid=b06ca37a6a6f13ebeaf5ae4e7ea80dd0")
+    .then(response => response.json())
+    .then(data => {
+        /*FORECAST VARIABLES*/
+        let dateAValue = data["dt_txt"];
+        let s = data["list"][0]["main"][0]["temp"];
+        let tempMaxAValue = data["list"][0]["main"][0]["temp_max"];
+        let tempMinAValue = data["list"][0]["main"][0]["temp_min"];
+        let tempFeelAValue = data["list"][0]["main"][0]["feels_like"];
+        let descAValue = data["list"][0]["weather"][0]["description"];
+        let windValue = data["list"][0]["wind"]["speed"];
+        /*VARIABLE CONVERSION*/
+        input.value ="";
+        main.innerHTML = nameValue;
+        dateA.innerHTML = "Date - "+dateAValue;
+        descA.innerHTML = "Description - "+descValue;
+        tempA.innerHTML = "Temperature - "+tempAValue+"°F";
+        minMaxA.innerHTML = tempMinAValue+"°Fmin/"+tempMaxAValue+"°Fmax";
+        feelsA.innerHTML = "but feels like "+tempFeelAValue+"°F";
+
+})
+
 .catch(err => alert("Wrong city info!"))
-    })
+})
